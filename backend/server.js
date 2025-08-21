c6e1de1f7a14297708de34db39cbdf14a6378727
Cam <76858431+dxzys@users.noreply.github.com>
2025-08-21 16:00:29 -0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const path = require('path');
 const fs = require('fs');
 const { createEvents } = require('ics');
-const moment = require('moment');
+const moment = require('moment-timezone');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
